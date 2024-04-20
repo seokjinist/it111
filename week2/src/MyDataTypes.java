9b3c2ba25896fd92b611081f04a8aba188e6124f
Thao Doan <103588189+seokjinist@users.noreply.github.com>
2024-04-19 20:33:25 -0700

    Add files via upload

---
+++ week2/src/MyDataTypes.java
@@ -0,0 +1,27 @@
+public class MyDataTypes {
+    public static void main(String[]args){
+
+        short   number1   = 32_000;
+        int     number5   = 2_000_000;
+        long    number2   = 2_000_000_000L;
+        float   number3   = 30.67F;
+        double  number4   = 50.654336;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+
+boolean dry = false;
+
+if (dry){
+    System.out.println("Time to hang up those shirts!!");
+} else {
+    System.out.println("We will have to wait a minute!!");
+}
+
+char best = 'A';
+        System.out.println(++best);
+    }
+}
---
+++ week2/src/MyVariable1.java
@@ -0,0 +1,36 @@
+public class MyVariable1 {
+    public static void main (String[]args) {
+
+String name = "Thao";
+    int hours = 40;
+        System.out.println(name);
+        System.out.println(hours);
+
+        String building, tree, lake, ocean;
+        tree = "Redwood";
+        building = "Empire State";
+        lake = "Lake Superior";
+        ocean = "Pacific Ocean";
+
+//        String building = "Empire State", tree = "Maple", lake = "Lake Superior", ocean = "Pacific Ocean";
+
+int weeks, decades, months, years;
+weeks = 4;
+decades = 10;
+months = 3;
+years = 5;
+
+//         int weeks = 4, decades = 10, months = 3, years = 5;
+
+        System.out.println(tree);
+        System.out.println(building);
+        System.out.println(lake);
+        System.out.println(ocean);
+        System.out.println(weeks);
+        System.out.println(decades);
+        System.out.println(months);
+        System.out.println(years);
+
+
+    }
+}
