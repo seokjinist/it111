f405d2876eadaa5fab68d7591c248b39eac1c25d
Thao Doan <103588189+seokjinist@users.noreply.github.com>
2024-04-26 21:51:56 -0700

    Add files via upload

--- LitertoGallonConverter.java
+++ LitertoGallonConverter.java
@@ -7,14 +7,14 @@ public class LitertoGallonConverter {
         for (gal = 1; gal <= 100; gal = gal + 4) {
             lit = (gal * 3.79);
             lit = Math.round(lit * 100.0) / 100.0;
-            System.out.println(lit + " Liters = " + gal + " Gallons.");
+            System.out.println(lit + " Liters = " + gal + " Gallon" + (gal != 1 ? "s" : "") + ".");
             counter++;
 
             // every 5th line, prints a blank line
             if (counter == 5) {
                 System.out.println("");
                 counter = 0; // resets the line counter
-                
+
             }
         }
 
