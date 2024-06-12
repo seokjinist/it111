a59ecf006132c96298e7558e2f5abe6ed0af2dd8
Thao Doan <103588189+seokjinist@users.noreply.github.com>
2024-06-12 15:07:28 -0700

    Add files via upload

--- dailySpecials.java
+++ dailySpecials.java
@@ -13,7 +13,8 @@ public class dailySpecials {
 
         System.out.println("Please enter a day of the week excluding weekends (Monday - Friday only!)");
 
-        while (true) {
+        boolean validDay = false;
+        while (!validDay) {
             specials = input.next();
 
             if (specials.equalsIgnoreCase("Saturday") || specials.equalsIgnoreCase("Sunday")) {
@@ -25,41 +26,64 @@ public class dailySpecials {
                 case "monday" -> {
                     coffee = "Latte";
                     price = 4.95;
+                    validDay = true;
                 }
                 case "tuesday" -> {
                     coffee = "Frappuccino";
                     price = 5.95;
+                    validDay = true;
                 }
                 case "wednesday" -> {
                     coffee = "Cappuccino";
                     price = 4.35;
+                    validDay = true;
                 }
                 case "thursday" -> {
                     coffee = "Regular Joe";
-                    price = 2.95;
+                    price = 1.0;
+                    validDay = true;
                 }
                 case "friday" -> {
                     coffee = "Green Tea Latte";
                     price = 6.95;
+                    validDay = true;
                 }
                 default -> {
                     System.out.println("Invalid day entered. Please enter a valid weekday.");
-                    continue;
                 }
             }
-            break;
         }
 
         System.out.println(capitalizeFirstLetter(specials) + "'s coffee of the day is a " + coffee + " and the price will be $" + price);
         System.out.println("How many " + coffee + "s would you like to order?");
-        quantity = input.nextInt();
+
+        while (true) {
+            if (input.hasNextInt()) {
+                quantity = input.nextInt();
+                break;
+            } else {
+                System.out.println("Please enter a valid number for quantity!");
+                input.next(); // Clear invalid input
+            }
+        }
 
         if (quantity == 0) {
-            System.out.println("Looks like you don't like " + coffee + "s! So sad!!!");
+            System.out.println("Looks like you don't like " + coffee + "s! So sad!!!"); // if they order O drinks
         } else if (quantity == 1) {
-            System.out.println("Looks like you will be ordering only 1 " + coffee + " today!");
+            System.out.println("Looks like you will be ordering only 1 " + coffee + " today totalling $" + String.format("%.2f", price * quantity) + " dollars!");
+        } else if (quantity <= 4 ) {
+            System.out.println(+ quantity + " " + coffee + "s  have been ordered totalling $" + String.format("%.2f", price * quantity) + " dollars!");
         } else {
-            System.out.println(quantity + " " + coffee + "s have been ordered totalling $" + String.format("%.2f", price * quantity) + " dollars!");
+            double totalCost = price * quantity;
+            if (quantity >= 10) {  // 10 and over drinks
+                totalCost *= 0.80; // A 20% discount
+                System.out.println("A bigger group discount! Your regular price is " + String.format("%.2f", price * quantity) + " dollars!");
+
+        } else if (quantity >= 5) { // 5 and over drinks
+                totalCost *= 0.90; // A 10% discount
+                System.out.println("Looks like you qualify for a group discount! Your regular price is " + String.format("%.2f", price * quantity) + " dollars!");
+            }
+            System.out.println("You have ordered " +quantity + " " + coffee + "s but will be only charged " + String.format("%.2f", totalCost) + " dollars!");
         }
 
         input.close();
